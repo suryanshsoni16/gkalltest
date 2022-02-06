4dbaec0635ca754c1eb94e98453163b988ec989a
tigerbhai16 <93959349+tigerbhai16@users.noreply.github.com>
2022-02-06 17:11:33 +0530

    Add files via upload

---
+++ test6feb(7)/script.js
@@ -0,0 +1,55 @@
+const addButton = document.getElementById("add");
+const notes = JSON.parse(localStorage.getItem("notes"));
+
+const updateLocalStorage = () => {
+  const notesText = document.querySelectorAll("textarea");
+  const notes = [];
+  notesText.forEach((note) => notes.push(note.value));
+  localStorage.setItem("notes", JSON.stringify(notes));
+};
+
+const addNewNote = (text = "Edit Heading:-") => {
+  
+  const note = document.createElement("div");
+  note.classList.add("note");
+  note.innerHTML = `
+  <div class="tools">
+  
+        <h2>yOur blOg...${1}</h2>
+        <button class="edit">edit</button>
+        <button class="delete">Delete</button>
+  </div>
+  
+
+  <div class="main ${text ? "" : "hidden"}"></div>
+  <textarea class="${text ? "hidden" : ""}"></textarea>`;
+
+  const editButton = note.querySelector(".edit");
+  const deleteButton = note.querySelector(".delete");
+  const main = note.querySelector(".main");
+  const textArea = note.querySelector("textarea");
+  textArea.value = text;
+  main.innerHTML = marked(text);
+
+  deleteButton.addEventListener("click", () => {
+    note.remove();
+    updateLocalStorage();
+  });
+  editButton.addEventListener("click", () => {
+    
+    main.classList.toggle("hidden");
+    textArea.classList.toggle("hidden");
+  });
+  textArea.addEventListener("input", (e) => {
+    const { value } = e.target;
+    main.innerHTML = marked(value);
+    updateLocalStorage();
+  });
+  document.body.appendChild(note);
+};
+
+addButton.addEventListener("click", () => addNewNote());
+
+if (notes) {
+  notes.forEach((note) => addNewNote(note));
+}
